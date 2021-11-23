07932e65dbeb8cbc3c92c79e8b58188cd36c6975
D-Justice <justice.damian96@gmail.com>
2021-11-24 06:52:27 +1100

    CodeAlong Compelete

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,8 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+courses = doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC").css('.title-oE5vT4')
+courses.each do |course|
+    puts course.text.strip
+end
